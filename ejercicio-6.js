b6565f2c210137c22718396f44ee6ceb399bb56d
camilo-OG <camilo.osorio.gsach.cl>
2023-06-15 15:36:35 -0400

    Ejercicios basicos v3

--- ejercicio-6.js
+++ ejercicio-6.js
@@ -1,7 +1,7 @@
 let num1 = new Number (window.prompt("Veamos que numero es mayor, ingrese el primer número"));
 let num2 = new Number (window.prompt("ingrese el segundo numero"));
 if (num1 > num2) {
-    console.log(`El número mayor es ${num1}`)
+    console.log(`El número ${num1} es mayor que ${num2}`)
 } else {
-    console.log(`El número mayor es ${num2}`)
+    console.log(`El número ${num2} es mayor que ${num1} `)
 };
